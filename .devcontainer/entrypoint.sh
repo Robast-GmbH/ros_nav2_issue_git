51607e8f039cc74f6ea91dfc2d5999570a050a6b
TipluJacob <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>
2022-01-12 14:14:44 +0100

    first draft for dockerfile

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#mkdir -p ~/rviz2_ws/src
+#cd ~/rviz2_ws/src
+#git clone https://github.com/ros2/rviz.git
+#cp -r ./resource_retriever ~/rviz2_ws/src/rviz/rviz_rendering/include/
+#colcon build --merge-install
+cd ~/../../workspaces/Robast_RosTheron
+#sudo dos2unix shell_setup.sh
+sudo /startup.sh
