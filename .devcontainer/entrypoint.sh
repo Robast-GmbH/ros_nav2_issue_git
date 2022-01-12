b3cb4995d895aa790c76e7fb74eaf1962d5dd22e
Tobi <t.alscher@robast.de>
2022-01-12 15:34:04 +0100

    init turtlebot setup

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-#mkdir -p ~/rviz2_ws/src
-#cd ~/rviz2_ws/src
-#git clone https://github.com/ros2/rviz.git
-#cp -r ./resource_retriever ~/rviz2_ws/src/rviz/rviz_rendering/include/
-#colcon build --merge-install
-cd ~/../../workspaces/Robast_RosTheron
-#sudo dos2unix shell_setup.sh
-sudo /startup.sh
+# wget https://raw.githubusercontent.com/ROBOTIS-GIT/turtlebot3/ros2/turtlebot3.repos
+vcs import src < turtlebot3.repos
+
+colcon build --symlink-install
---
+++ start.sh
@@ -0,0 +1,6 @@
+echo 'source /workspaces/ros_nav2_issue_git/install/setup.bash' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/workspaces/ros_nav2_issue_git/src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models' >> ~/.bashrc
+echo 'export TURTLEBOT3_MODEL=waffle_pi' >> ~/.bashrc
+source ~/.bashrc
+
+ros2 launch turtlebot3_gazebo empty_world.launch.py
